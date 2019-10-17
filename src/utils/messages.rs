37444d37823c1924418e88e4de2167303fec975f
zzxx-husky <zzxx.is.me@gmail.com>
2019-10-17 23:28:50 +0800

    [Init Commit] Toy Example for Cycle Detection on Streaming Graph on Timely Dataflow
    The case now is detection two 6-length cycles with different time spans.

---
+++ src/utils/messages.rs
@@ -0,0 +1,98 @@
+extern crate abomonation;
+
+use abomonation::Abomonation;
+use std::cmp::Ordering;
+use std::hash::{Hasher, Hash};
+use timely::order::Product;
+
+#[derive(Debug)]
+pub struct FetchRequestTimestamp {
+  pub timestamp: Product<usize, u32>,
+}
+
+impl Ord for FetchRequestTimestamp {
+  // make the comparison reverse !!!
+  fn cmp(&self, other: &FetchRequestTimestamp) -> Ordering {
+    other.timestamp.cmp(&self.timestamp)
+  }
+}
+
+impl PartialOrd for FetchRequestTimestamp {
+  fn partial_cmp(&self, other: &FetchRequestTimestamp) -> Option<Ordering> {
+    other.timestamp.partial_cmp(&self.timestamp)
+  }
+}
+
+impl PartialEq for FetchRequestTimestamp {
+  fn eq(&self, other: &FetchRequestTimestamp) -> bool {
+    self.timestamp == other.timestamp
+  }
+}
+
+impl Eq for FetchRequestTimestamp {}
+
+#[derive(Clone, Debug)]
+pub struct UpdateRequest {
+  pub src: u32,
+  pub dst: u32,
+  pub is_addition: u32, // +1 -> add, -1 -> del
+}
+
+#[derive(Clone, Debug)]
+pub struct FetchRequest {
+  pub vertex_id: u32,
+  // one worker may detect multiple patterns, each has a different operator idx
+  pub worker_idx: usize,
+  // a pattern is being detected on multiple records
+  pub operator_idx: usize,
+  pub subgraph_idx: usize,
+  pub record_idx: usize,
+  pub time_span: usize,
+}
+
+
+#[derive(Clone, Debug)]
+pub struct FetchReply {
+  pub worker_idx: usize,
+  pub operator_idx: usize,
+  pub subgraph_idx: usize,
+  pub record_idx: usize,
+  pub vertex_id: u32,
+  pub neighbors: Vec<u32>,
+}
+
+impl Abomonation for UpdateRequest {}
+
+impl FetchRequest {
+  pub fn reply(&self, neighbors: Vec<u32>) -> FetchReply {
+    FetchReply {
+      worker_idx: self.worker_idx,
+      operator_idx: self.operator_idx,
+      subgraph_idx: self.subgraph_idx,
+      record_idx: self.record_idx,
+      vertex_id: self.vertex_id,
+      neighbors: neighbors,
+    }
+  }
+}
+
+impl Abomonation for FetchRequest {}
+
+impl Hash for FetchRequest {
+  fn hash<H>(&self, state: &mut H) where H: Hasher {
+    state.write_usize(self.worker_idx);
+    state.write_usize(self.operator_idx);
+    state.write_usize(self.record_idx);
+    state.finish();
+  }
+}
+
+impl PartialEq for FetchRequest {
+  fn eq(&self, other: &FetchRequest) -> bool {
+    self.worker_idx == other.worker_idx && self.operator_idx == other.operator_idx && self.record_idx == other.record_idx
+  }
+}
+
+impl Eq for FetchRequest {}
+
+impl Abomonation for FetchReply {}
---
+++ src/utils/subgraph.rs
@@ -0,0 +1,119 @@
+use super::messages::*;
+
+use std::collections::HashMap;
+
+struct Vertex {
+  level: u32,
+  neighbors: Vec<u32>,
+}
+
+pub struct CyclePattern {
+  // const
+  worker_idx: usize,
+  operator_idx: usize,
+  subgraph_idx: usize,
+  time_span: usize,
+  max_cycle_length: u32,
+  root: u32,
+
+  // non-const
+  request_id: usize,
+  graph: HashMap<u32, Vertex>,
+  pub num_pending_replies: usize,
+}
+
+impl CyclePattern {
+  pub fn create(worker_idx: usize, operator_idx: usize, subgraph_idx: usize, time_span: usize, max_cycle_length: u32) -> CyclePattern {
+    CyclePattern {
+      worker_idx: worker_idx,
+      operator_idx: operator_idx,
+      subgraph_idx: subgraph_idx,
+      time_span: time_span,
+      max_cycle_length: max_cycle_length,
+      root: 0,
+      request_id: 0,
+      graph: HashMap::new(),
+      num_pending_replies: 0,
+    }
+  }
+
+  pub fn add_starting_edge(&mut self, u: &UpdateRequest) -> Vec<FetchRequest> {
+    self.graph.insert(u.src, Vertex { level: 0, neighbors: vec!(u.dst) });
+    self.graph.insert(u.dst, Vertex { level: 1, neighbors: Vec::new() });
+    self.root = u.src;
+    let requests = vec!(FetchRequest {
+      vertex_id: u.dst,
+      worker_idx: self.worker_idx,
+      operator_idx: self.operator_idx,
+      subgraph_idx: self.subgraph_idx,
+      record_idx: self.request_id,
+      time_span: self.time_span,
+    });
+    self.request_id += 1;
+    self.num_pending_replies += requests.len();
+    requests
+  }
+
+  pub fn on_reply(&mut self, r: &FetchReply) -> Vec<FetchRequest> {
+//    println!("{}", r.vertex_id);
+    self.num_pending_replies -= 1;
+    {
+      match self.graph.get_mut(&r.vertex_id) {
+        Some(v) => {
+          v.neighbors = r.neighbors.clone();
+        }
+        None => panic!("Failed to find the vertex that makes the request")
+      }
+    }
+    let mut new_requests = Vec::new();
+    let mut new_vertices = Vec::new();
+    {
+      match self.graph.get(&r.vertex_id) {
+        Some(v) => {
+          if v.level < self.max_cycle_length {
+            for n in r.neighbors.iter() {
+              if !self.graph.contains_key(n) {
+                new_vertices.push((*n, Vertex { level: v.level + 1, neighbors: Vec::new() }));
+                new_requests.push(FetchRequest {
+                  vertex_id: *n,
+                  worker_idx: self.worker_idx,
+                  operator_idx: self.operator_idx,
+                  subgraph_idx: self.subgraph_idx,
+                  record_idx: self.request_id,
+                  time_span: self.time_span,
+                });
+                self.request_id += 1;
+              }
+            }
+          }
+        }
+        None => panic!("There is bug hiding somewhere.")
+      }
+    }
+    self.graph.extend(new_vertices);
+    self.num_pending_replies += new_requests.len();
+    new_requests
+  }
+
+  fn recursive_detect(&self, root_level: usize, imme_path: &mut Vec<u32>) {
+    if root_level < 5 {
+      for n in self.graph[&imme_path[root_level]].neighbors.iter() {
+        imme_path.push(*n);
+        self.recursive_detect(root_level + 1, imme_path);
+        imme_path.pop();
+      }
+    } else {
+      if self.graph[&imme_path[root_level]].neighbors.contains(&self.root) {
+        imme_path.push(self.root);
+        println!("Found: {:?}", imme_path);
+        imme_path.pop();
+      }
+    }
+  }
+
+  pub fn detect_cycles(&self) {
+    let mut imme_path = vec!(self.root);
+    self.recursive_detect(0, &mut imme_path);
+  }
+}
+
